010ca22df82f3173cce935b50b363a4243d96fb3
uniquepointer <uniquepointer@mailbox.org>
2021-10-05 23:22:54 -0500

    Drill X and x now move cursor to the end and x if used repeatedly does move the cursor downwards

--- drill.c
+++ drill.c
@@ -697,12 +697,23 @@ void drill_normal(int key, char *key_str) {
             drill_X_select = 0;
             break;
         case 'x':
-            drill_X_select ^= 1;
-            YEXE("select-lines");
+            if (drill_X_select)
+            {
+                YEXE("select-lines");
+                YEXE("cursor-down");
+                YEXE("select-lines");
+            }
+            else
+            {
+                YEXE("select-lines");
+            }
+            drill_X_select = 1;
+            YEXE("cursor-line-end");
             break;
         case 'X':
             if (!drill_X_select && !drill_visual_mode_bool) YEXE("select-lines");
             drill_X_select = 1;
+            YEXE("cursor-line-end");
             YEXE("cursor-down");
             break;
         case 's':
